c9706f6dd2c94c2d1002dce9a62fa3ee564af1f8
Chris Kielholz <Enorion@users.noreply.github.com>
2018-10-18 23:53:11 +0200

    Update SECore.php
    Adding LanguageHandler

--- core/classes/Base/SECore.php
+++ core/classes/Base/SECore.php
@@ -3,11 +3,14 @@ namespace Base;
 
 class SECore
 {
-    private $db;
+    private $db, $cookieHandler, $language;
     
     public function __construct()
     {
         $this->db = new Db();
+        $this->cookieHandler = new CookieHandler();
+        
+        $this->setLanguage();
         
         return true;
     }
@@ -15,5 +18,21 @@ class SECore
     public function Db()
     {
         return $this->db;
+    }
+    
+    private function setLanguage()
+    {
+        if ($this->cookieHandler->checkCookie("language")) {
+            $this->language =  $this->getCookieValue("language");    
+        } else {
+            $this->language = "en_EN";
+        }
+        
+        $this->updateLanguage();
+    }
+    
+    private function updateLanguage()
+    {
+        $language = new LanguageHandler($this->language);
     }
 }
