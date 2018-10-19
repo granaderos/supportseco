b57872f97a9db9ad967980a2c43d3900b5c19422
Chris Kielholz <Enorion@users.noreply.github.com>
2018-10-19 23:24:49 +0200

    Update LanguageHandler.php

--- core/classes/Base/LanguageHandler.php
+++ core/classes/Base/LanguageHandler.php
@@ -17,22 +17,26 @@ class LanguageHandler
         return false;
     }
     
+    /**
+     * Checking if a language exists as a translation yml file
+     *
+     * @author Enorion <enorion@supports.eco>
+     * @param string $languageCode
+     * @return bool
+     */
     private function checkLanguageCode($languageCode)
     {
         $directory = '/core/languages';
-        $files = array();
         $found = false;
-
         foreach (scandir($directory) as $file) {
             if ('.' === $file) continue;
             if ('..' === $file) continue;
-
             if ($file == $languageCode."yml") {
                 $found = true;
                 exit;
             }
         }
-        
+
         if ($found == true) {
             return true;
         } else {
@@ -40,29 +44,49 @@ class LanguageHandler
         }
     }
     
+    /**
+     * Parsing the YML file for the scope language
+     *
+     * @author Enorion <enorion@supports.eco>
+     * @return bool
+     */
     private function parseYml()
     {
         $this->strings = yaml_parse_file('/core/languages/'.$this->languageCode.'.yml');
-        
+
         return true;
     }
     
+    /**
+     * Getting a scope translation string by key and binding placeholders if passed
+     *
+     * @author Enorion <enorion@supports.eco>
+     * @param string $key
+     * @param array $bindings
+     * @return bool|mixed
+     */
     public function getByKey($key,$bindings = array())
     {
         if (in_array($key,$this->strings)) {
             $baseString = $this->strings[$key];
             if (!empty($bindings)) {
-                 foreach($bindings AS $bindingName => $bindingValue) {
-                     $baseString = str_replace("%".$bindingName."%",$bindingValue,$baseString);    
-                 }
+                foreach($bindings AS $bindingName => $bindingValue) {
+                    $baseString = str_replace("%".$bindingName."%",$bindingValue,$baseString);
+                }
             }
-            
+
             return $baseString;
         } else {
-            return false;    
+            return false;
         }
     } 
     
+    /**
+     * Returns the full array of translations from the scope
+     *
+     * @author Enorion <enorion@supports.eco>
+     * @return array
+     */
     public function getAll()
     {
         return $this->strings;
