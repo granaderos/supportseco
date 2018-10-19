a7eeeb49f669657f996043f90b3bac993e5f2997
Chris Kielholz <Enorion@users.noreply.github.com>
2018-10-19 15:36:39 +0200

    Create CookieHandler.php
    Creating basic CookieHandler class

---
+++ core/classes/Base/CookieHandler.php
@@ -0,0 +1,57 @@
+<?php
+namespace Base;
+
+class CookieHandler
+{
+    private $cookies
+
+    public function __construct()
+    {
+        $this->cookies = $_COOKIES;
+        $this->sanitizeCookies();
+        
+        return true;
+    }
+    
+    /**
+    * Sanitizing the Cookies Array to prevent Injection
+    *
+    * @author Enorion <enorion@supports.eco>
+    * @return boolean
+    */
+    private function sanitizeCookies()
+    {
+        foreach($this->cookies as $name => $cookie) {
+            $this->cookies[$name] = htmlspecialchars($cookie);
+        }
+        
+        return true;
+    }
+    
+    /**
+    * Checks a given name exists in the Cookies Array
+    *
+    * @author Enorion <enorion@supports.eco>
+    * @return boolean
+    */
+    public function checkCookie($name)
+    {
+        if (array_key_exists($name,$this->cookies) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+    * Returns the value of a given name from the Cookies Array
+    *
+    * @author Enorion <enorion@supports.eco>
+    * @todo Implement $this->checkCookie($name)
+    * @return mixed
+    */
+    public function getCookieValue($name)
+    {
+        return $this->cookies[$name];
+    }
+}   
